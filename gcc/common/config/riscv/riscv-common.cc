2841d856f3c20b22ce003f291febd6e7ff6891f4
Arthur Cohen <arthur.cohen@embecosm.com>
2025-10-14 12:45:09 +0200

    ttrocc: Add extension and base misc builtins
    This commit adds basic support for the ttrocc RISC-V extension as well as
    two builtins, __builtin_riscv_ttrocc_noc_fence() and __builtin_riscv_ttrocc_dbg_postcode()
    
    gcc/ChangeLog:
    
    	* common/config/riscv/riscv-common.cc: Add ttrocc extension.
    	* config/riscv/riscv-builtins.cc: Insert ttrocc def file, define rocc availability
    	macro.
    	* config/riscv/riscv-ftypes.def: Define required types for all builtins of the spec.
    	* config/riscv/riscv.md: Include ttrocc machine description file.
    	* config/riscv/riscv.opt: Add ttrocc extension flag.
    	* config/riscv/ttrocc.def: New file.
    	* config/riscv/ttrocc.md: New file.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/ttrocc_dbg_postcode.c: New test.
    	* gcc.target/riscv/ttrocc_noc_fence.c: New test.

--- gcc/common/config/riscv/riscv-common.cc
+++ gcc/common/config/riscv/riscv-common.cc
@@ -446,6 +446,8 @@ static const struct riscv_ext_version riscv_ext_version_table[] =
   {"xtttensixwh", ISA_SPEC_CLASS_NONE, 1, 0},
   {"xttwhraw", ISA_SPEC_CLASS_NONE, 1, 0},
 
+  {"xttrocc", ISA_SPEC_CLASS_NONE, 1, 0},
+
   /* Terminate the list.  */
   {NULL, ISA_SPEC_CLASS_NONE, 0, 0}
 };
@@ -1784,6 +1786,7 @@ static const riscv_ext_flag_table_t riscv_ext_flag_table[] =
 
   RISCV_EXT_FLAG_ENTRY ("xtttensixbh", x_riscv_tt_subext, MASK_XTT_TENSIX_BH),
   RISCV_EXT_FLAG_ENTRY ("xtttensixwh", x_riscv_tt_subext, MASK_XTT_TENSIX_WH),
+  RISCV_EXT_FLAG_ENTRY ("xttrocc", x_riscv_tt_subext, MASK_XTT_ROCC),
 
   {NULL, NULL, NULL, 0}
 };
