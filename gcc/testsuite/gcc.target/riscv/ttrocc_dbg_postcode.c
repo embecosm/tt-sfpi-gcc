a98dbf8a3933db2a2ae90e5c13921902423150bf
Arthur Cohen <arthur.cohen@embecosm.com>
2025-10-20 13:12:36 +0200

    ttrocc: Fix predicates for input operands
    gcc/ChangeLog:
    
    	* config/riscv/ttrocc.md: Add "r" constraint to all arguments passed through
    	registers.
    	(ttrocc_cmdbuf): New predicate for direct integer cmdbuf values (0..=1)
    	(ttrocc_register): New predicate for direct integer register values (0..=63)
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/ttrocc_dbg_postcode.c: Pass value as a register and test for this
    	instead.
    	* gcc.target/riscv/ttrocc_invalid_operands.c: New test.

--- gcc/testsuite/gcc.target/riscv/ttrocc_dbg_postcode.c
+++ gcc/testsuite/gcc.target/riscv/ttrocc_dbg_postcode.c
@@ -2,5 +2,5 @@
 // { dg-options "-mcpu=tt-wh" }
 
 int main(void) {
-  __builtin_riscv_ttrocc_dbg_postcode(1); // { dg-final { "scan-assembler" "tt\.rocc\.dbg_postcode\t1" } }
+  __builtin_riscv_ttrocc_dbg_postcode(1); // { dg-final { "scan-assembler" "tt\.rocc\.dbg_postcode" } }
 }
