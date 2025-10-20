3dc3ab54ef3deb548e3773b87b3970c6d1724899
Arthur Cohen <arthur.cohen@embecosm.com>
2025-10-20 12:44:55 +0200

    ttrocc: Fix return value register constraints and add them to the mnemonics
    gcc/ChangeLog:
    
    	* config/riscv/ttrocc.md: Add return register as the first argument in the
    	generated assembly.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/ttrocc_unused_arguments.c: Fix test to account for this.

--- gcc/testsuite/gcc.target/riscv/ttrocc_unused_arguments.c
+++ gcc/testsuite/gcc.target/riscv/ttrocc_unused_arguments.c
@@ -6,6 +6,6 @@ main (void)
 {
   __builtin_riscv_ttrocc_addrgen_wr_reg (0, 4, 15); // { dg-final { "scan-assembler" "tt\.rocc\.addrgen_wr_reg\tx0,.*,.*,.*,x0" } }
 
-  unsigned long rd0 = __builtin_riscv_ttrocc_addrgen_rd_reg (0, 4); // { dg-final { "scan-assembler" "tt\.rocc\.addrgen_rd_reg\tx0,.*,.*,x0,x0" } }
+  unsigned long rd0 = __builtin_riscv_ttrocc_addrgen_rd_reg (0, 4); // { dg-final { "scan-assembler" "tt\.rocc\.addrgen_rd_reg\t.*,.*,.*,x0,x0" } }
 
 }
